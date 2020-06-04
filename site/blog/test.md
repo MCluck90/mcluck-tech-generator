cc58361b6d56c9d0a2e85d147f1d79509b4e4354
Mike Cluck <mike-cluck@pluralsight.com>
2020-06-04 15:04:45 -0600

    Initial commit

---
+++ site/blog/test.md
@@ -0,0 +1,8 @@
+----
+title: Test Title
+date: 2020-06-04
+----
+
+# Test
+
+Look at me! I'm a test file!
---
+++ src/main.ts
@@ -0,0 +1,85 @@
+import fs from 'fs'
+import mkdirp from 'mkdirp'
+import path from 'path'
+import rimraf from 'rimraf'
+import MarkdownIt from 'markdown-it'
+import YAML from 'yaml'
+import { FrontMatter } from './types'
+
+const _sitePath = path.join(__dirname, '../site')
+const sourcePaths = {
+  blog: path.join(_sitePath, 'blog'),
+  blogTemplate: path.join(_sitePath, 'blog', 'template.html')
+}
+const _distPath = path.join(__dirname, '../dist')
+const distPaths = {
+  blog: path.join(_distPath, 'blog')
+}
+
+if (fs.existsSync(_distPath)) {
+  for (const file of fs.readdirSync(_distPath)) {
+    rimraf.sync(file)
+  }
+}
+
+for (const p of Object.values(distPaths)) {
+  mkdirp.sync(p)
+}
+
+// Generate the blog entries
+const htmlTemplate = fs.readFileSync(sourcePaths.blogTemplate).toString()
+const files = fs
+  .readdirSync(sourcePaths.blog)
+  .filter(file => path.extname(file) === '.md')
+  .map(fileName => {
+    const filePath = path.join(sourcePaths.blog, fileName)
+    const contents = fs.readFileSync(filePath).toString()
+    return {
+      fileName,
+      contents
+    }
+  })
+
+function assertValidFrontMatter(
+  frontMatter: Partial<FrontMatter>
+): asserts frontMatter is FrontMatter {
+  if (frontMatter.title === undefined) {
+    throw new TypeError(`Must provide a title in the front-matter`)
+  }
+  if (frontMatter.date === undefined) {
+    throw new TypeError(`Must provide a date in the front-matter`)
+  }
+}
+
+for (const { fileName, contents } of files) {
+  const md = new MarkdownIt()
+  const filePath = path.join(sourcePaths.blog, fileName)
+  let frontMatter: FrontMatter | null = null
+  md.use(require('markdown-it-front-matter'), function (fm: string) {
+    const potentialFrontMatter: Partial<FrontMatter> = YAML.parse(fm)
+    try {
+      assertValidFrontMatter(potentialFrontMatter)
+      frontMatter = potentialFrontMatter
+    } catch (e) {
+      console.error(`${filePath}: ${e.message}`)
+      process.exit(1)
+    }
+  })
+
+  const postContents = md.render(contents)
+
+  if (frontMatter === null) {
+    throw new TypeError(
+      `Must provide front matter for ${filePath}. See types.ts for required fields`
+    )
+  }
+  frontMatter = frontMatter as FrontMatter // Goofy hack because TS can't properly infer here
+
+  const htmlFileName = fileName.replace('.md', '.html')
+  const outPath = path.join(distPaths.blog, htmlFileName)
+  const html = htmlTemplate
+    .replace('{{title}}', frontMatter.title)
+    .replace('{{date}}', frontMatter.date)
+    .replace('{{post}}', postContents)
+  fs.writeFileSync(outPath, html)
+}
