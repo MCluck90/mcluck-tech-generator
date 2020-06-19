ddd8b152fd010ba4cf82f66af699cae50203ed1f
Mike Cluck <mike-cluck@pluralsight.com>
2020-06-19 09:15:55 -0600

    Move homepage metadata to a JSON file

--- src/main.ts
+++ src/main.ts
@@ -57,24 +57,11 @@ for (const file of nonBlogSiteFiles) {
       }
 
       const template = data.toString()
+      const homepage = require('./homepage.json')
       const result = Handlebars.compile(template)({
+        siteRoot: variables.siteRoot,
         blogPosts,
-        toolsAndToys: [
-          { title: 'What Should I Eat?', permalink: `${variables.siteRoot}/what-should-i-eat` },
-          { title: 'Field of View Demo', permalink: `${variables.siteRoot}/vision-demo` }
-        ],
-        linksThatILike: [
-          {
-            title: 'Weird Wide Webring',
-            url: 'https://weirdwidewebring.net/',
-            description: 'The web needs a little more weird. These sites are helping.'
-          },
-          {
-            title: 'Webring',
-            url: 'https://webring.xxiivv.com/',
-            description: 'Maintained by @neauoire'
-          }
-        ]
+        ...homepage
       })
       fs.writeFile(path.join(distPath, 'index.html'), result, err => {
         if (err) {
